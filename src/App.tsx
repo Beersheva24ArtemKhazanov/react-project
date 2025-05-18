b8413f0b6e37a3d4f8eb65ce948ef685d4b45c10
Artem Khazanov <tulenka04@gmail.com>
2025-05-18 12:20:56 +0300

    TimersWall

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
 import React from 'react'
+
 import './App.css'
-import Timer from './components/Timer'
+import TimersWall from './components/timers-wall/TimersWall.tsx'
+
 const App: React.FC = () => {
+  
   return (
-    <div className="container typography"><Timer city="London"/>
-    <Timer city="Delhi"></Timer>
-    <Timer city="Paris"></Timer>
-    <Timer city="Moscow"></Timer>
-    <Timer city="Jerusalem"></Timer>
-    <Timer city="Yekaterinburg "></Timer>
-    <Timer city="Kyiv"></Timer>
-    <Timer city="Beirut"></Timer>
-    <Timer city="Krasnoyarsk"></Timer>
-    <Timer city="Vladivostok"></Timer>
-    <Timer city="Los_Angeles"></Timer>
-    <Timer city="Mexico"></Timer>
-    <Timer city="Buenos_Aires"></Timer>
-    <Timer city="Toronto"></Timer>
-    <Timer city="Sydney"></Timer>
-    <Timer city="Kamchatka"></Timer>
-   </div>
+    <TimersWall></TimersWall>
   )
 }
 
--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,11 +1,12 @@
-import useTimer from "../hooks/useTimer.ts";
+import useTimer from "../hooks/useTimer";
 import timezones from "../../config/timezones.json";
+import { useMemo } from "react";
 interface Props {
   interval?: number;
   city: string;
 }
 const Timer: React.FC<Props> = ({ interval = 1000, city }) => {
-  const timeZone = getTimeZone(city);
+  const timeZone = useMemo(() => getTimeZone(city),[city]);
   const { hour, minute, second } = useTimer(
     new Date(new Date().toLocaleString(undefined, { timeZone })),
     interval
@@ -14,6 +15,7 @@ const Timer: React.FC<Props> = ({ interval = 1000, city }) => {
   
     function getTimeZone(city: string): string | undefined {
       let res;
+      console.log("getTimeZone")
       const timeZoneObj = timezones.find((obj) =>
         JSON.stringify(obj).includes(city)
       );
